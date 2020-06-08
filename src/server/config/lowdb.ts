2cd1bbc8d1ca7a45fd66fbc884e08f214b4fbcf8
Maciej Ziehlke <mac@dmio.co>
2020-06-08 23:59:25 +0100

    DB file can be URL, Mokksy will load it from the server

--- src/server/config/lowdb.ts
+++ src/server/config/lowdb.ts
@@ -1,30 +1,46 @@
 import fp from 'fastify-plugin';
 import fs from 'fs';
+import isUrl from 'is-url';
 import low from 'lowdb';
 import chalk from 'chalk';
+import got from 'got';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Memory = require('lowdb/adapters/Memory');
 
 export default fp(async (server, { sourceFile }) => {
-  // make sure that the file exists
-  if (!fs.existsSync(sourceFile)) {
-    console.log('\n');
-    console.log(chalk.bold.red(`Oops, the database file '${sourceFile}' doesn't exist. Bye!`));
-    console.log('\n');
-    process.exit(0);
-  }
-
-  // TODO: Give users option, maybe they want us to change the data file?
-  // For now all changes are in-memory only.
-
   // load working copy file to memory
   const adapter = new Memory();
   const db = await low(adapter);
 
-  // load defaults to our database
-  const data = fs.readFileSync(sourceFile, 'utf-8');
-  db.defaults(JSON.parse(data)).write();
+  // check if sourceFile is URL, if it is, get data and save as defaults
+  if (isUrl(sourceFile)) {
+    try {
+      const body = await got.get(sourceFile).json();
+      db.defaults(body).write();
+    } catch {
+      console.log('\n');
+      console.log(chalk.bold.red(`Oops, the data we got from '${sourceFile}' is not JSON. Bye!`));
+      console.log('\n');
+      process.exit(0);
+    }
+  }
 
+  // string is not URL, so open it as a file
+  else {
+    // make sure that the file exists
+    if (!fs.existsSync(sourceFile)) {
+      console.log('\n');
+      console.log(chalk.bold.red(`Oops, the database file '${sourceFile}' doesn't exist. Bye!`));
+      console.log('\n');
+      process.exit(0);
+    }
+
+    // load file as defaults to our database
+    const data = fs.readFileSync(sourceFile, 'utf-8');
+    db.defaults(JSON.parse(data)).write();
+  }
+
+  // decorate 'fastify' object
   server.decorate('lowDb', db);
 });
